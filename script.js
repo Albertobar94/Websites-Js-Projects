87d0d1dea1ee8f5b959fc1908dfe50718ab4ce49
Alberto <devalbertobarboza@gmail.com>
2020-12-07 19:30:18 -0300

    hello world

---
+++ script.js
@@ -0,0 +1,6 @@
+(() => {
+    // Github token
+    // vscode://vscode.github-authentication/did-authenticate?windowid=1&code=10113e56521390f60d18&state=43775755-e033-4ea1-a04a-95d2080f7806
+    return
+}
+)();
